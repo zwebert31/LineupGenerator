f2ae86e0325fff744ccd521dc6ccf4faa318cc5b
Zachary Webert <zwebert@zacharys-macbook-pro.local>
2015-09-29 17:46:15 -0400

    Initial Commit

---
+++ LineupCreator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LineupCreator
+//
+//  Created by Zachary Webert on 9/29/15.
+//  Copyright © 2015 Zachary Webert. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
